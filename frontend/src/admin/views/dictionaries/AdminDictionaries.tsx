723896d281c2dbb70c92c14ea3f4cb2edbdf514f
pawelmat142 <pawelmat142@gmail.com>
2025-09-16 10:26:24 +0200

    buttons

--- frontend/src/admin/views/dictionaries/AdminDictionaries.tsx
+++ frontend/src/admin/views/dictionaries/AdminDictionaries.tsx
@@ -1,15 +1,14 @@
 import Buton from "../../../global/components/controls/Buton";
-import { BtnModes } from "../../../global/interface/controls.interface";
 
 const AdminDictionaries: React.FC = () => {
 
+    const onAddDictionary = () => {
+        console.log('add dictionary');
+    }
+
     return (<div className="flex flex-1 flex-col gap-2 justify-center items-center">
 
-        <Buton>Elo</Buton>
-        <Buton mode={BtnModes.SECONDARY}>Elo</Buton>
-        <Buton mode={BtnModes.PRIMARY_TXT}>Elo</Buton>
-        <Buton mode={BtnModes.SECONDARY_TXT}>Elo</Buton>
-        <Buton mode={BtnModes.WARNING}>Elo</Buton>
+        <Buton onClick={onAddDictionary}>Add dictionary</Buton>
 
     </div>)
 }
