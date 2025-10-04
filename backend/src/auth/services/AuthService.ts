e254474da8f193de89d4e1d0285cce879b505b74
pawelmat142 <pawelmat142@gmail.com>
2025-10-04 15:45:02 +0200

    obsluga firebase auth

--- backend/src/auth/services/AuthService.ts
+++ backend/src/auth/services/AuthService.ts
@@ -4,6 +4,8 @@ import { RegisterFormDto, RegisterFormResponse } from '@shared/dto/AuthDto';
 import { AuthValidators } from '@shared/validators/AuthValidator';
 import { ToastException } from 'global/exceptions/ToastException';
 import { FirebaseConfig } from '../../config/FirebaseConfig';
+import { UserRecord } from 'firebase-admin/auth';
+import { PopupException } from 'global/exceptions/PopupException';
 
 // TODO 
 // czy moge z przegladarki pobrac jakis unikalny klucz/id/numer charakterystyczny dla urzadzenia?
@@ -19,8 +21,6 @@ export class AuthService {
   }
 
   public async registerForm(dto: RegisterFormDto): Promise<RegisterFormResponse> {
-    this.logger.log('registerForm', dto);
-
     const validationErrorKey = AuthValidators.validateRegisterForm(dto);
     if (validationErrorKey) {
       throw new ToastException(validationErrorKey, this);
@@ -28,18 +28,26 @@ export class AuthService {
 
     // Registration logic with Firebase Auth
     try {
-      const userRecord = await this.firebaseAuth.createUser({
+      const userRecord: UserRecord = await this.firebaseAuth.createUser({
         email: dto.email,
         password: dto.password,
       });
 
-      this.logger.log('Firebase user created', userRecord.uid);
+      this.logger.log('Firebase user created', JSON.stringify(userRecord, null, 2));
 
       return {
         success: true,
         message: 'validation.success',
       };
     } catch (err: any) {
+      const firebaseErrorCode = err?.errorInfo?.code;
+      if (firebaseErrorCode) {
+        if (firebaseErrorCode === 'auth/email-already-exists') {
+          throw new PopupException(`validation.firebaseError.emailInUse`, this)
+        }
+        this.logger.error(err);
+      }
+      // TODO obsluga email zajety itp
       throw new ToastException('validation.firebaseError', this);
     }
   }
