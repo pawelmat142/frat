ba3905f0a7e9dcc0f16be5db87af412ac310c413
pawelmat142 <pawelmat142@gmail.com>
2025-12-13 17:49:20 +0100

    deploy with hosting by nginx - works local

--- frontend/src/global/services/http.ts
+++ frontend/src/global/services/http.ts
@@ -9,8 +9,8 @@ import { Path } from '../../path';
 import { FirebaseAuth } from 'auth/services/FirebaseAuth';
 
 // const API_URL = "http://78.159.81.140:3100/api"
-// const API_URL = "http://78.159.81.140/api"
-const API_URL = "http://localhost:3100/api"
+const API_URL = "http://78.159.81.140/api"
+// const API_URL = "http://localhost:3100/api"
 
 export const queryClient = new QueryClient();
 
