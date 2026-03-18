2551885235ce4f10488a4e123e2ea3234452c5df
pawel <pawelmat142@gmail.com>
2026-03-18 07:27:01 +0100

    header animation when navigate

--- frontend/src/global/components/GlobalHeader.tsx
+++ frontend/src/global/components/GlobalHeader.tsx
@@ -1,5 +1,6 @@
 import { useGlobalContext } from 'global/providers/GlobalProvider';
 import { useTranslation } from 'react-i18next';
+import { AnimatePresence, motion } from 'framer-motion';
 import DesktopMenu from './DesktopMenu';
 
 const GlobalHeader: React.FC = () => {
@@ -12,23 +13,32 @@ const GlobalHeader: React.FC = () => {
         <header className={`header relative`}>
             <nav className={` h-full ${stickyHeader ? 'sticky-header' : ''}`}>
 
-                <div className="header-content p-container">
+                <AnimatePresence mode="wait">
+                    <motion.div
+                        key={globalCtx?.state?.title || 'empty'}
+                        className="header-content p-container"
+                        initial={{ opacity: 0, x: 12 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: -12 }}
+                        transition={{ duration: 0.15 }}
+                    >
 
-                    <div className="header-content-left">
-                        {globalCtx?.state?.leftBtn}
+                        <div className="header-content-left">
+                            {globalCtx?.state?.leftBtn}
 
-                        {globalCtx?.state?.title && (
-                            <div className="header-title ml-2 btn-font pri-text">
-                                {t(globalCtx?.state?.title)}
-                            </div>
-                        )}
-                    </div>
+                            {globalCtx?.state?.title && (
+                                <div className="header-title ml-2 btn-font pri-text">
+                                    {t(globalCtx.state.title)}
+                                </div>
+                            )}
+                        </div>
 
-                    <div className='flex items-center'>
-                        {!!globalCtx.isDesktop && (<DesktopMenu />)}
-                        {globalCtx?.state?.rightBtn}
-                    </div>
-                </div>
+                        <div className='flex items-center'>
+                            {!!globalCtx.isDesktop && (<DesktopMenu />)}
+                            {globalCtx?.state?.rightBtn}
+                        </div>
+                    </motion.div>
+                </AnimatePresence>
 
             </nav>
         </header>
