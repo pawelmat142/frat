696fb0204cb52e1d75a01465cd6e3cb6cbcf6af6
pawel <pawelmat142@gmail.com>
2026-04-01 07:12:10 +0200

    fixed header of position selector

--- frontend/src/global/components/selector/position/PositionSelectorContent.tsx
+++ frontend/src/global/components/selector/position/PositionSelectorContent.tsx
@@ -2,12 +2,12 @@ import { useState, useRef, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 import Button from '../../controls/Button';
-import { BtnModes } from 'global/interface/controls.interface';
-import PositionSelectorSearchbar from './PositionSelectorSearchbar';
+import { BtnModes, FloatingInputModes } from 'global/interface/controls.interface';
 import GoogleMapsLoader from 'global/utils/GoogleMapsLoader';
 import { GoogleMapService } from 'global/services/GoogleMapService';
 import { Position, GeocodedPosition } from '@shared/interfaces/MapsInterfaces';
 import { AppConfig } from '@shared/AppConfig';
+import FloatingPlaceSearch from 'global/components/controls/FloatingPlaceSearch';
 
 interface PositionSelectorContentProps {
     initialPosition?: Position;
@@ -65,7 +65,7 @@ const PositionSelectorContent: React.FC<PositionSelectorContentProps> = ({
         await GoogleMapsLoader.load(apiKey);
 
         if (!mapRef.current) return;
-        
+
         const map = new google.maps.Map(mapRef.current, {
             center: position,
             zoom: initialZoom,
@@ -117,18 +117,26 @@ const PositionSelectorContent: React.FC<PositionSelectorContentProps> = ({
         <div className="position-selector-popup-overlay fixed inset-0 z-50 flex items-center justify-center bg-opacity-50">
             <div className="position-selector-popup secondary-bg rounded-lg shadow-xl w-full h-full md:w-[90%] md:h-[90%] max-w-6xl flex flex-col">
 
-                <PositionSelectorSearchbar
-                    mapInstanceRef={mapInstanceRef.current}
-                    updatePosition={updatePosition}
-                    onCancel={onCancel}
-                    displaValue={selectedPosition?.fullAddress}
-                />
+                <div className='p-2 primary-bg'>
+                    <FloatingPlaceSearch
+                        className='w-full'
+                        mode={FloatingInputModes.THIN}
+                        name="freeText"
+                        displayValue={selectedPosition?.fullAddress}
+                        onSelect={result => updatePosition({ lat: result.lat, lng: result.lng })}
+                        mapInstanceRef={mapInstanceRef.current}
+                        label={t('employeeProfile.form.freeText')}
+                        fullWidth
+                    />
+                </div>
 
                 <div className="position-selector-map-container flex-1 relative">
+
+
                     <div ref={mapRef} className="w-full h-full" />
                 </div>
 
-                <div className="position-selector-footer px-4 py-2 flex gap-4 justify-between border-t">
+                <div className="position-selector-footer px-4 py-2 flex gap-4 justify-between border-t ">
                     <Button onClick={() => onChange(null)} mode={BtnModes.ERROR_TXT}>
                         {t('common.reset')}
                     </Button>
